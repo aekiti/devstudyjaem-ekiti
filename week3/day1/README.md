fbcbf84108e40e09798afeb30af74f5000f36286
Emmanuel Joseph <emmanueljet774@gmail.com>
2020-02-12 06:53:01 +0100

    Week 3 Day 1

---
+++ week3/day1/README.md
@@ -0,0 +1,13 @@
+# Week 3 Day 1 - [#devstudyjaem](https://twitter.com/search?q=%23devstudyjaem)
+
+## aeprogect
+
+<a href="https://aeproject.gitbook.io/aeproject/" target="_blank" title="aeproject">https://aeproject.gitbook.io/aeproject/</a>
+
+## Book Library aepp
+
+<a href="https://aekiti.github.io/devstudyjaem-ekiti/week3/day1/" target="_blank" title="book">https://aekiti.github.io/devstudyjaem-ekiti/week3/day1/</a>
+
+## Settings
+
+* On mobile (or developer mode mobile device view) go to <a href="https://base.aepps.com/#/browser/aekiti.github.io/devstudyjaem-ekiti/week2/day2/" target="_blank" title="base aepp todo">https://base.aepps.com/#/browser/aekiti.github.io/devstudyjaem-ekiti/week2/day2/</a> to see books from the librarian
---
+++ week3/day1/js/index.js
@@ -0,0 +1,90 @@
+const contractSource=`
+contract BookLibrary=
+
+  record book = {
+    title:string,
+    author:string,
+    image:string,
+    url:string }
+    
+  record state = {
+    books:map(string,book),
+    totalBookCount:int,
+    librarian:address }
+
+  entrypoint init() = {
+    books={},
+    totalBookCount=0,
+    librarian = Call.caller }
+
+  function requirement(exp : bool, err : string) =
+    if(!exp)
+        abort(err)
+
+  entrypoint onlylibrarian() : bool =
+    requirement(state.librarian ==ak_2FuPqTvcRSQaPznxboDKSQRJUpmBUGJz7nrWV1CmySbAf2FsDE, "Not Authorized")
+    true
+
+  entrypoint getTotalBookCount()=
+    state.totalBookCount
+  
+  stateful entrypoint addBook(title':string, author':string, image':string, url':string, isbn:string) =
+    onlylibrarian()
+    let newBook = {title=title',author=author',image=image', url=url'}
+    let index = getTotalBookCount() + 1
+    put(state{books[isbn]=newBook,totalBookCount=index})
+    
+  entrypoint getBook(isbn)=
+    state.books[isbn]
+    
+  entrypoint getAllBooks()=
+    state.books
+`;
+const contractAddress="ct_2EUr87rhXVAYPuBEJwzCvHukEZhAdrtC4vWbZcnsGrJQ7zytyy";
+
+let client = null, contractInstance = null, allBooks, bookArray = [], totalBook = 0;
+
+function renderBooks() {
+  let bookCard = $('#bookCard').html();
+  Mustache.parse(bookCard);
+  let rendered = Mustache.render(bookCard, {bookArray});
+  $('#allBooks').html(rendered);
+}
+
+window.addEventListener('load', async () => {
+  client = await Ae.Aepp();
+  contractInstance = await client.getContractInstance(contractSource, {contractAddress});
+  totalBook = (await contractInstance.methods.getTotalBookCount()).decodedResult;
+  document.getElementById("totalBooks").innerHTML = '(' + totalBook + ')';
+
+  allBooks = (await contractInstance.methods.getAllBooks()).decodedResult;
+
+  for (let i in allBooks) {
+
+    const book = (await contractInstance.methods.getBook(i)).decodedResult;
+
+    bookArray.push({
+      bookImage: book.image,
+      bookTitle: book.title,
+      bookAuthor: book.author,
+      bookIsbn: i,
+      bookUrl: book.url,
+    })
+  }
+
+  renderBooks();
+  $("#loader").hide();
+});
+
+$('#add-book').click(async function(){
+  $("#loader").show();
+  const title = ($('#btitle').val()),
+        author = ($('#bauthor').val()),
+        image = ($('#bimage').val()),
+        isbn = ($('#bisbn').val()),
+        url = ($('#burl').val());
+
+  await contractInstance.methods.addBook(title, author, image, url, isbn);
+
+  location.reload();
+});
